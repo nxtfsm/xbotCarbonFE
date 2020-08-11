c359c3a2f609656d49d5cf4ee0e38cae8b92d21f
nxtfsm <sysadmin@xaviermicronesia.org>
2020-08-11 16:21:05 -0500

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Expects a local .env file with Mapbox API key, formatted as:
+MB_owner=
+MB_token=
--- src/js/loadDataObjects.js
+++ src/js/loadDataObjects.js
@@ -2,4 +2,4 @@
 import * as sectionConfigData from '../data/sectionConfigs.json'
 import { initSections } from './models/topLevelSectionObjects'
 
-export const loadSectionObjects = () => {return initSections(sectionConfigData.default)}
+export const loadSectionObjects = () => { return initSections(sectionConfigData.default)}
